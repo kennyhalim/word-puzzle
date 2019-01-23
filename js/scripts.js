0b2fd6f0785f8994748fdf1dbacb361e9037a585
Kenny Halim and maddy haldeman <maddy@>
2019-01-23 10:21:53 -0800

    add finishing touches, clean up JS, and toggle result with buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,28 @@ $(document).ready(function(){
 
     var strings = $("#string").val();
     $("#output").empty();
+    $(".userinput").hide();
+    $(".result").show();
+    $("#answerbox").text(strings);
     for (var index = 0; index < strings.length; index+=1) {
+      if (vowels.includes(strings[index].toLowerCase())){
 
-    if (vowels.includes(strings[index].toLowerCase())){
-
-      //alert("in array");
-      $("#output").append("-");
-    } else {
-      //alert("not in array");
-      $("#output").append(strings[index]);
+        //alert("in array");
+        $("#output").append("_");
+      } else {
+        //alert("not in array");
+        $("#output").append(strings[index]);
+      }
     }
 
-  }
-
     event.preventDefault();
+  });
+
+  $("button#showresult").click(function(){
+    $("#answer").toggle();
+  });
+
+  $("button#goBack").click(function(){
+    location.reload(); //refreshes the page
   });
 });
